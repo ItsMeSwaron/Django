68d0efb9a9b2152fda1a06d84a7dad065712b161
ItsMeSwaron <swaron3214@gmail.com>
2024-11-07 22:09:23 +0600

    module 1 codes

---
+++ project_1/app_1/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return HttpResponse('This is home of app_1')
+
+def courses(request):
+    return HttpResponse('This is courses of app_1.')
+
+def about(request):
+    return HttpResponse('This is about page of app_1.')
